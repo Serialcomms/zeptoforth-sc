de2021a7610b67ff9c37f70a272f0af934aafc0b
serialcomms <serialcomms@outlook.com>
2024-11-27 15:14:37 +0000

    Create test-message.fs

---
+++ test-message.fs
@@ -0,0 +1,7 @@
+  create test-message
+
+   \   Z      e      p      t      o      f      o      r      t      h             U      S      B  <cr> , <lf>
+
+   $5A c, $65 c, $70 c, $74 c, $6F c, $66 c, $6F c, $72 c, $74 c, $68 c, $20 c, $55 c, $53 c, $42 c, $0D c, $0A c,
+
+   here test-message - cell align, constant test-message-size
